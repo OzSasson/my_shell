29b363e3cc16ad8b846bd3e62aab671118cbf750
OzSasson <121007993+OzSasson@users.noreply.github.com>
2023-09-22 14:52:54 +0300

    Update my_shell.py
    added more internal commands

--- my_shell.py
+++ my_shell.py
@@ -1,4 +1,5 @@
-import os
+import glob
+import os, shutil, datetime
 
 func_dic = {
     'cd': 'Displays the name of or changes the current directory.',
@@ -10,9 +11,7 @@ func_dic = {
     'set': 'Sets environment variables.',
     'cls': 'Clears the screen.',
     'help': 'Shows what every command does'
-
 }
-
 
 
 def print_dict():
@@ -22,10 +21,10 @@ def print_dict():
 
 
 def change_dir(dir):
-   try:
-       os.chdir(dir)
-   except Exception:
-       print(f"Directory '{dir}' not found.")
+    try:
+        os.chdir(dir)
+    except Exception:
+        print(f"Directory '{dir}' not found.")
 
 
 def helP(com):
@@ -36,44 +35,148 @@ def helP(com):
         print(f'Command "{com}" not found in the dictionary.')
 
 
-
 def exiT():
-    pass
+    exit()
 
 
-def seT():
-    pass
+def diR(path):
+    try:
+        entries = os.scandir(path)
+        for entry in entries:
+            info = entry.name
+            if entry.is_dir():
+                info = "[" + info + "]"
+            file_size = entry.stat().st_size
+            if file_size < 1024:
+                file_size_str = str(file_size) + ' bytes'
+            else:
+                file_size_str = str(file_size / 1024) + ' KB'
+            file_time = datetime.datetime.fromtimestamp(entry.stat().st_mtime).strftime('%Y-%m-%d %H:%M %p')
+            print(f"{file_time} {file_size_str} {info}")
+    except FileNotFoundError:
+        print(f"Directory '{path}' not found.")
+    except PermissionError:
+        print(f"Permission denied to access directory '{path}'.")
 
 
-def diR():
-    pass
+def move(src_path, dst_path):
+    try:
+        if not os.path.exists(src_path):
+            print(f"Error: {src_path} does not exist.")
+            return
+
+        shutil.move(src_path, dst_path)
+    except Exception as e:
+        print(f"Cant move")
+
+
+def copy(src_path, dst_path):
+    try:
+        if not os.path.exists(src_path):
+            print(f"Error: {src_path} does not exist.")
+            return
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dst_path)
+        elif os.path.isdir(src_path):
+            shutil.copytree(src_path, dst_path)
+    except Exception as e:
+        print(f"Cant copy")
+
+
+def cls():
+    print("\033c", end="")
+
+
+def deL(path):
+    matching_paths = glob.glob(path)
+
+    if not matching_paths:
+        print(f'No files or folders match the pattern: {path}')
+        return
+
+    for path in matching_paths:
+        try:
+            if os.path.isfile(path):
+                os.remove(path)
+                print(f'Deleted file: {path}')
+            elif os.path.isdir(path):
+                shutil.rmtree(path)
+                print(f'Deleted folder and its contents: {path}')
+            else:
+                print(f'Error: "{path}" is neither a file nor a directory.')
+        except Exception as e:
+            print(f'Error deleting {path}: {e}')
+
+
+def seT(var=None):
+    if var:
+        if '=' not in var:
+            var = var.upper()
+            res = os.environ.get(var)
+            if res:
+                print(f'{var}={res}')
+            else:
+                print(f'Environment variable {var} not defined')
+        else:
+            var = var.split('=')
+            os.environ[var[0]] = var[1]
+
+    else:
+        for var, value in os.environ.items():
+            print(f"{var}={value}")
+
 
 def check_command(inp):
-    ls=inp.split(" ")
-    if ls[0]=='cd':
+    ls = inp.split(" ")
+    if ls[0] == 'cd':
         if len(ls) == 1:
             print(os.getcwd())
             return
         change_dir(ls[1])
-    elif ls[0]=='help':
-        if len(ls)>1:
+    elif ls[0] == 'help':
+        if len(ls) > 1:
             return helP(ls[1])
         else:
             print_dict()
-    elif ls[0]=='exit':
+    elif ls[0] == 'exit':
         return exiT()
-    elif ls[0]=='set':
-        return seT()
-    elif ls[0]=='dir':
-        return diR()
+    elif ls[0] == 'dir':
+        if len(ls) > 1:
+            return diR(ls[1])
+        else:
+            return diR(os.getcwd())
+    elif ls[0] == 'move':
+        if len(ls) != 3:
+            print('Not enough parameters')
+            return
+        move(ls[1], ls[2])
+    elif ls[0] == 'copy':
+        if len(ls) != 3:
+            print('Not enough parameters')
+            return
+        copy(ls[1], ls[2])
+    elif ls[0] == 'cls':
+        cls()
+    elif ls[0] == 'del':
+        if len(ls) != 2:
+            print('Not enough parameters')
+            return
+        deL(ls[1])
+    elif ls[0] == 'set':
+        if len(ls) == 2:
+            seT(ls[1])
+            return
+        seT()
     else:
         print(1)
+
 
 def main():
     while True:
         cmd = input(f'{os.getcwd()} :)')
-        cmd=cmd.lower().strip()
+        cmd = cmd.lower().strip()
         check_command(cmd)
 
-if _name_ == '__main__':
+
+if __name__ == '__main__':
     main()
